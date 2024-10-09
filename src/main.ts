b4f7177ba1c2e5cd0feb1fb40cd0ea2c33106cd9
lmurayam <lmurayam@ucsc.edu>
2024-10-09 00:23:17 -0700

    Added Button - Step One Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const mainButton = document.createElement("button");
+mainButton.innerHTML = '🍜';
+app.append(mainButton);
