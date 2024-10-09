8c9ace58f05f2935b06de5f721cea052efee4dfc
lmurayam <lmurayam@ucsc.edu>
2024-10-09 00:49:38 -0700

    Automatic Clicking - Step Three Complete

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,23 @@ header.innerHTML = gameName;
 app.append(header);
 
 const mainButton = document.createElement("button");
-mainButton.innerHTML = '🍜';
+mainButton.innerHTML = "🍜";
 app.append(mainButton);
 
-let counter : number = 0;
+let counter: number = 0;
 const counterText = document.createElement("div");
 app.append(counterText);
-refreshCounterText();
 
-mainButton.addEventListener('click',()=>{
-    counter+=1;
-    refreshCounterText();
-});
-
-function refreshCounterText() {
+function updateCounter(x:number) {
+    counter+=x;
     counterText.innerHTML = `Number of Bowls: ${counter}`;
 }
+
+updateCounter(0);
+
+mainButton.addEventListener("click", () => {
+    updateCounter(1);
+});
+
+setInterval(updateCounter,1000,1);
+
