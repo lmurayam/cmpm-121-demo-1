9e828f01b920a8bf114a7261efbda0f64526300c
lmurayam <lmurayam@ucsc.edu>
2024-10-09 01:42:22 -0700

    Cont Growth - Step Four Complete

--- src/main.ts
+++ src/main.ts
@@ -17,16 +17,37 @@ let counter: number = 0;
 const counterText = document.createElement("div");
 app.append(counterText);
 
-function updateCounter(x:number) {
-    counter+=x;
-    counterText.innerHTML = `Number of Bowls: ${counter}`;
+function updateCounter(x: number) {
+  counter += x;
+  counterText.innerHTML = `Number of Bowls: ${counter.toFixed(2)}`;
 }
 
 updateCounter(0);
 
 mainButton.addEventListener("click", () => {
-    updateCounter(1);
+  updateCounter(1);
 });
 
-setInterval(updateCounter,1000,1);
+
+// const startTime : number =performance.now();
+// let frames : number = 0;
+// let totalTime : number = 0;
+
+// function addFractionAvg(){
+//     frames++;
+//     totalTime = (performance.now()-startTime)/1000;
+//     const fps : number = (frames/totalTime);
+//     updateCounter(1/fps);
+//     requestAnimationFrame(addFractionAvg);
+// }
+
+let lastTime : number = performance.now();
+
+function addFraction(){
+    const elapsedTime : number = (performance.now()-lastTime)/1000;
+    lastTime=performance.now();
+    updateCounter(elapsedTime);
+    requestAnimationFrame(addFraction);
+}
+requestAnimationFrame(addFraction);
 
